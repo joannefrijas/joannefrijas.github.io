fff0cf6c63fb324aeb9122771e80ba4be40609c1
joannefrijas <142962912+joannefrijas@users.noreply.github.com>
2023-08-29 21:17:41 -0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,3 @@
-document.write("<span style='font-family: Times New Roman; font-style: strong; color: red;'>xy</span>");
-document.write("<span style='font-family: Arial; color: blue;'>12</span>");
-document.write("<span style='font-family: Impact; font-style: italics; color: green;'>89</span>");
+document.write("<span style='font-family: Times New Roman; font-style: strong; color: red;'>xy &nbsp;</span>");
+document.write("<span style='font-family: Arial; color: blue;'>12 &nbsp;</span>");
+document.write("<span style='font-family: Impact; font-style: italics; color: green;'>89 &nbsp;</span>");
